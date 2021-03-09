f410620fc73cb973147c130173a0ac04fb3e94c5
Andrés Nuñez <andresanunez98@gmail.com>
2021-03-09 13:44:03 -0500

    commit tree 6c90b64f37a05c630b50b79a5f2eb35b2717a53f

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,27 @@
 class BankAccount
+attr_reader :name
+attr_accessor :status, :balance
+
+def initialize(name)
+@name = name
+@status = "open"
+@balance = 1000
+end
+
+def deposit(sum)
+@balance = self.balance + sum
+end
+
+def display_balance
+    "Your balance is $#{self.balance}."
+end
+
+def valid?
+  @status == "open" && @balance > 0
+end
+
+def close_account
+self.status = "closed"
+end
 
 end
